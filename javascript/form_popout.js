a7d78d0feb0fff431dcbfc32671e7307da4f68e1
KST <khantsi.23@ichat.sp.edu.sg>
2025-02-06 00:11:24 +0800

    5.2.2025 Sleep

--- javascript/form_popout.js
+++ javascript/form_popout.js
@@ -1,23 +1,28 @@
-
+const targets = document.getElementsByClassName("card");
 function openFormL(){
-    document.getElementById("login").style.display = "block";
-    /*let targets = document.querySelectorAll(".card");
-    targets.forEach(element => {
-        element.style.display= "none";
-    });*/
+    document.getElementById("login").style.display = "flex";
+    for (let i=0;i<targets.length;i++){
+        targets[i].style.display= "none";
+    }
 }
 
 function closeForm(){
     document.getElementById("login").style.display = "none";
     document.getElementById("signup").style.display = "none";
     document.getElementById("about").style.display = "none";
-    targets.forEach(element => {
-        element.style.display= "block";
-    });
+    for (let i=0;i<targets.length;i++){
+        targets[i].style.display= "block";
+    }
 }
 function openFormR(){
-    document.getElementById("signup").style.display = "block";
+    document.getElementById("signup").style.display = "flex";
+    for (let i=0;i<targets.length;i++){
+        targets[i].style.display= "none";
+    }
 }
 function openAbout(){
-    document.getElementById("about").style.display = "block";
+    document.getElementById("about").style.display = "flex";
+    for (let i=0;i<targets.length;i++){
+        targets[i].style.display= "none";
+    }
 }

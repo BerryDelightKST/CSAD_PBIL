d86f954f3e372ea31afb8475630edd3538e747c8
lewistan2 <isntlewis@gmail.com>
2025-02-09 17:06:28 +0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,6 +2,13 @@
 session_start();
 require 'config.php';
 
+if (isset($_POST['logout'])) {
+        session_unset();
+        session_destroy();
+        header("Location: login.php");
+        exit;
+    }
+
 if (!isset($_SESSION['user_id'])) {
     header('Location: login.php');
     exit;
